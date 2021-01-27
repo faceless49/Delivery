d2bc885dd46809372932645810064b571cae7e85
Egor K <digital.zeus@mail.ru>
2021-01-27 18:45:41 +0300

    make modals

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+'use strict'
+
+
+let loginIn = document.querySelector('.header__btn--sign')
+let overlay = document.querySelector('.overlay')
+let modalLogin = document.getElementById('login');
+let closeBtn = document.querySelector('.modal__closebtn');
+
+loginIn.addEventListener('click', function(){
+    overlay.style.display = 'block'
+    // modalLogin.style.display = 'block'
+})
+
+closeBtn.addEventListener('click', function(){
+    overlay.style.display = 'none'
+    // modalLogin.style.display = 'none'
+})
+
+window.addEventListener('click', function(event){
+    if (event.target === overlay){
+        // modalLogin.style.display = 'none'
+        overlay.style.display = 'none'
+    }
+})
+
